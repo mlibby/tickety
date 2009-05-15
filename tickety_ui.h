c6e1efaaf1c46b4abffaac6580399134b9ec5bf6
Michael <mcl@shihtzu.(none)>
2009-05-15 14:17:30 -0500

    applying GPL officially

--- tickety_ui.h
+++ tickety_ui.h
@@ -1,3 +1,22 @@
+/*
+ *   Copyright 2009, Michael C. Libby
+ *
+ *   This file is part of Tickety.
+ *
+ *   Tickety is free software: you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tickety is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tickety.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _TICKETY_UI_H
 #define	_TICKETY_UI_H
 
